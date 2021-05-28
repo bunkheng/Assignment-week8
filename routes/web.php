fee7f1be2db3775e173713eb68a26e1bcc34af0b
bunkheng <kenmasters765@gmail.com>
2021-05-28 21:21:19 +0700

    Add Home page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/post', function () {
+    return view('post');
+});
